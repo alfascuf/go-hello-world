322c02c0fb1b56c2aca0097ffea938367d897ea8
alfascuf <shakromolodoj9002@gmail.com>
2025-09-04 15:31:58 +0300

    Вторая программа в Go

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+
+	results := make([]string, t)
+
+	for i := 0; i < t; i++ {
+		var s string
+		fmt.Fscan(in, &s)
+
+		n := len(s)
+		idx := n - 1
+		for j := 0; j < n-1; j++ {
+			if s[j] < s[j+1] {
+				idx = j
+				break
+			}
+		}
+
+		results[i] = s[:idx] + s[idx+1:]
+	}
+
+	for _, res := range results {
+		fmt.Fprintln(out, res)
+	}
+}
